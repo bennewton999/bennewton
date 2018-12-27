4c26fe8ab12e0ef225f18dcd2a356a85940e3b88
Ben Newton <bennewton999@gmail.com>
2018-12-27 15:59:42 -0500

    add image

---
+++ index.js
@@ -0,0 +1,20 @@
+const cfonts = require('cfonts');
+const imageToAscii = require('image-to-ascii');
+
+imageToAscii(
+  'https://benenewton.com/logos/ben.png',
+  {
+    // pixels: '█',
+    // size: { height: '50%' },
+    size_options: { screen_size: { height: 50, width: 50 } },
+    fit_screen: false
+  },
+  (err, converted) => {
+    console.log(err || converted);
+  }
+);
+
+cfonts.say('Ben Newton', {
+  //   font: 'huge',
+  colors: ['white', 'blue', 'red']
+});
